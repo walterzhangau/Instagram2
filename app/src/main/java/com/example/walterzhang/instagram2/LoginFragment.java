a849a8c788e01e99af7bfee6e828b63cc0ed4751
gusUmelb <42226629+gusUmelb@users.noreply.github.com>
2018-09-09 02:26:30 +1000

    Improve login page layout, add error handling and call application on successful login

--- app/src/main/java/com/example/walterzhang/instagram2/LoginFragment.java
+++ app/src/main/java/com/example/walterzhang/instagram2/LoginFragment.java
@@ -64,7 +64,7 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.activity_login, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
