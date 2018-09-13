2bf44c25216daa9586cebe4b1de791378aa849d5
gusUmelb <42226629+gusUmelb@users.noreply.github.com>
2018-09-13 19:17:17 +1000

    Changes to user feed page and list of likes

--- app/src/main/java/com/example/walterzhang/instagram2/Home/HomeFragment.java
+++ app/src/main/java/com/example/walterzhang/instagram2/Home/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.walterzhang.instagram2.Home;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: starting...");
         View view =inflater.inflate(R.layout.fragment_post,container,false);
         return view;
     }
