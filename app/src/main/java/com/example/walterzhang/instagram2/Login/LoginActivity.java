714bcd008daf257e0cb09798e04782b4983a38b3
Harshit Sethi <hsethi2709@gmail.com>
2018-09-20 14:07:32 +1000

    Added Firebase Dependencies and Libraries

--- app/src/main/java/com/example/walterzhang/instagram2/Login/LoginActivity.java
+++ app/src/main/java/com/example/walterzhang/instagram2/Login/LoginActivity.java
@@ -1,14 +1,10 @@
 package com.example.walterzhang.instagram2.Login;
 
 import android.content.Intent;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -32,8 +28,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-
-import java.util.function.ToDoubleBiFunction;
 
 public class LoginActivity extends AppCompatActivity {
 
