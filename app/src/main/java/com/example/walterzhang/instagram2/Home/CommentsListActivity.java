f8d1441ca9806073f229bf83500f4af58f043a83
gusUmelb <42226629+gusUmelb@users.noreply.github.com>
2018-09-22 21:02:06 +1000

    Add back navigation from Comments page

--- app/src/main/java/com/example/walterzhang/instagram2/Home/CommentsListActivity.java
+++ app/src/main/java/com/example/walterzhang/instagram2/Home/CommentsListActivity.java
@@ -2,11 +2,14 @@ package com.example.walterzhang.instagram2.Home;
 
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.walterzhang.instagram2.R;
 import com.example.walterzhang.instagram2.dummy.DummyContent;
@@ -17,21 +20,25 @@ public class CommentsListActivity extends AppCompatActivity implements fragment_
     private static final String TAG = "CommentsListActivity";
     private static final int ACTIVITY_NUM = 0;
 
+    private Class<?> cls;
     private Context mContext = CommentsListActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_comments);
         Log.d(TAG, "onCreate: started ");
 
         setupBottomNavigationView();
 
         android.app.Fragment commentFragment = new fragment_comment_list();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.relLayout2, commentFragment); // give your fragment container id in first parameter
+        transaction.replace(R.id.relLayout_activity_comments, commentFragment); // give your fragment container id in first parameter
         //transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
         transaction.commit();
+
+        TextView snippet_top_title = (TextView)findViewById(R.id.snippet_top_return);
+        snippet_top_title.setText("Comments");
     }
 
     /**
@@ -49,5 +56,18 @@ public class CommentsListActivity extends AppCompatActivity implements fragment_
 
     public void onListFragmentInteraction(DummyContent.DummyItem uri) {
         //you can leave it empty
+    }
+
+    public void onFragmentInteraction()
+    {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
+    }
+
+    public void onBackClicked(View view)
+    {
+        Log.d(TAG, "onBackClicked...");
+        cls = HomeActivity.class;
+        onFragmentInteraction();
     }
 }
