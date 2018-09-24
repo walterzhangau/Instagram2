bc91b523885a46c7495a9efdfce7cc5a34af6dbd
Mingshun Chu <mingshunc@student.unimelb.edu.au>
2018-09-24 20:02:57 +1000

    Created gallery and photo fragments

---
+++ app/src/main/java/com/example/walterzhang/instagram2/Share/GalleryFragment.java
@@ -0,0 +1,27 @@
+package com.example.walterzhang.instagram2.Share;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.walterzhang.instagram2.R;
+
+/**
+ * Created by mingshunc on 24/9/18.
+ */
+
+public class GalleryFragment extends Fragment {
+    private static final String TAG = "GalleryFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: starting...");
+        View view = inflater.inflate(R.layout.fragment_gallery,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/walterzhang/instagram2/Share/ShareActivity.java
+++ app/src/main/java/com/example/walterzhang/instagram2/Share/ShareActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -13,6 +15,7 @@ import android.view.MenuItem;
 import com.example.walterzhang.instagram2.R;
 import com.example.walterzhang.instagram2.utils.BottomNavigationViewHelper;
 import com.example.walterzhang.instagram2.utils.Permissions;
+import com.example.walterzhang.instagram2.utils.SectionsPagerAdapter;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 /**
@@ -25,20 +28,37 @@ public class ShareActivity extends AppCompatActivity{
     private static final int ACTIVITY_NUM = 2;
     private static final int VERIFY_PERMISSIONS_REQUEST = 1;
 
+    private ViewPager mViewPager;
+
     private Context mContext = ShareActivity.this;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_share);
         Log.d(TAG, "onCreate: started ");
 
         if (checkPermissionArray(Permissions.PERMISSIONS)) {
-            // do something
+            setupViewPager();
         } else {
             verifyPermissions(Permissions.PERMISSIONS);
         }
 
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
+    }
+
+    private void setupViewPager() {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new GalleryFragment());
+        adapter.addFragment(new PhotoFragment());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabsBottom);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        tabLayout.getTabAt(0).setText(R.string.gallery);
+        tabLayout.getTabAt(1).setText(R.string.photo);
     }
 
     /**
