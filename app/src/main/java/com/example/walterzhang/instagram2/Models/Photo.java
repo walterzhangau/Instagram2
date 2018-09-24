b6fc3dfd4acc5d81e66cbd77d80be20e2c6975cc
gusUmelb <42226629+gusUmelb@users.noreply.github.com>
2018-09-24 23:01:36 +1000

    Add photo model

---
+++ app/src/main/java/com/example/walterzhang/instagram2/Models/Photo.java
@@ -0,0 +1,51 @@
+package com.example.walterzhang.instagram2.Models;
+
+public class Photo {
+    private String photo_id;
+    private String date_taken;
+    private String image_path;
+    private String user_id;
+
+    public Photo() {
+
+    }
+
+    public Photo(String photo_id, String date_taken, String image_path, String user_id) {
+        this.photo_id = photo_id;
+        this.date_taken = date_taken;
+        this.image_path = image_path;
+        this.user_id = user_id;
+    }
+
+    public String getPhoto_id() {
+        return photo_id;
+    }
+
+    public String getDate_taken() {
+        return date_taken;
+    }
+
+    public String getImage_path() {
+        return image_path;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setPhoto_id(String photo_id) {
+        this.photo_id = photo_id;
+    }
+
+    public void setDate_taken(String date_taken) {
+        this.date_taken = date_taken;
+    }
+
+    public void setImage_path(String image_path) {
+        this.image_path = image_path;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
