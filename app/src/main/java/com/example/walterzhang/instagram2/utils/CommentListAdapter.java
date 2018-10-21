0606deb88f8ab904728d51420205fd2361b1a4f5
Walter Zhang <walterz@student.unimelb.edu.au>
2018-10-21 12:47:39 +1100

    website removed from user profile bug fixes

--- app/src/main/java/com/example/walterzhang/instagram2/utils/CommentListAdapter.java
+++ app/src/main/java/com/example/walterzhang/instagram2/utils/CommentListAdapter.java
@@ -3,6 +3,7 @@ package com.example.walterzhang.instagram2.utils;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,23 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.example.walterzhang.instagram2.Models.Comment;
+import com.example.walterzhang.instagram2.Models.UserAccountSettings;
+import com.example.walterzhang.instagram2.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -21,7 +39,7 @@ public class CommentListAdapter extends ArrayAdapter<Comment> {
     private int layoutResource;
     private Context mContext;
 
-    public CommentListAdapter(@NonNull Context context, int resource, @NonNull Comment[] objects) {
+    public CommentListAdapter(@NonNull Context context, int resource,  @NonNull List<Comment> objects) {
         super(context, resource, objects);
 
         mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -43,22 +61,93 @@ public class CommentListAdapter extends ArrayAdapter<Comment> {
             convertView = mInflater.inflate(layoutResource, parent, false);
             holder = new ViewHolder();
 
-            //TODO: FIX LAYOUT BEFORE UNCOMMENTING THESE LINES
-//            holder.username = (TextView) convertView.findViewById(R.id.comment_username);
-//            holder.comment = (TextView) convertView.findViewById(R.id.comment);
-//            holder.timestamp = (TextView) convertView.findViewById(R.id.comment_time_posted);
-//            holder.profileImage = (CircleImageView) convertView.findViewById(R.id.comment_profile_image);
 
+            holder.comment = (TextView) convertView.findViewById(R.id.comment);
+            holder.username = (TextView) convertView.findViewById(R.id.comment_username);
+            holder.timestamp = (TextView) convertView.findViewById(R.id.comment_time_posted);
+            holder.profileImage = (CircleImageView) convertView.findViewById(R.id.comment_profile_image);
             convertView.setTag(holder);
         }
         else {
             holder = (ViewHolder) convertView.getTag();
         }
 
+
+        //set the comment
         holder.comment.setText(getItem(position).getComment());
 
-        //TODO: TIMESTAMP DIFFERENCE
+        //set the timestamp difference
+        String timestampDifference = getTimestampDifference(getItem(position));
+        if(!timestampDifference.equals("0")){
+            holder.timestamp.setText(timestampDifference + " d");
+        }else{
+            holder.timestamp.setText("today");
+        }
+
+        //set the username and profile image
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference
+                .child(mContext.getString(R.string.dbname_user_account_settings))
+                .orderByChild(mContext.getString(R.string.field_user_id))
+                .equalTo(getItem(position).getUser_id());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for ( DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+                    holder.username.setText(
+                            singleSnapshot.getValue(UserAccountSettings.class).getUsername());
+
+                    ImageLoader imageLoader = ImageLoader.getInstance();
+
+                    imageLoader.displayImage(
+                            singleSnapshot.getValue(UserAccountSettings.class).getProfile_photo(),
+                            holder.profileImage);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: query cancelled.");
+            }
+        });
+
 
         return convertView;
     }
+
+    /**
+     * Returns a string representing the number of days ago the post was made
+     * @return
+     */
+    private String getTimestampDifference(Comment comment){
+        Log.d(TAG, "getTimestampDifference: getting timestamp difference.");
+
+        String difference = "";
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+        sdf.setTimeZone(TimeZone.getTimeZone("Australia/VICTORIA"));//google 'android list of timezones'
+        Date today = c.getTime();
+        sdf.format(today);
+        Date timestamp;
+        final String photoTimestamp = comment.getDate_created();
+        try{
+            timestamp = sdf.parse(photoTimestamp);
+            difference = String.valueOf(Math.round(((today.getTime() - timestamp.getTime()) / 1000 / 60 / 60 / 24 )));
+        }catch (ParseException e){
+            Log.e(TAG, "getTimestampDifference: ParseException: " + e.getMessage() );
+            difference = "0";
+        }
+        return difference;
+    }
 }
+
+
+
+
+
+
+
+
+
+
